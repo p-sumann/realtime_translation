ff8367b34674fe25922dc8a261d6b447b50256fb
Suman Paudel <dastonsuman1997@gmail.com>
2025-01-12 23:56:04 +0545

    add endpoint for translation

--- app/fastapi_app.py
+++ app/fastapi_app.py
@@ -1,16 +1,21 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
+from schemas import TranslationRequest, TaskResponse, TranslationStatus
 app = FastAPI()
 
 @app.get("/")
 async def hello():
     return 'Hello World!'
 
-@app.get("/inputs/{number}")
-async def read_input(number):
-    return {'hello': number}
-    
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*']
+)
 
-@app.get("/models/{model}")
-async def read_model(model: str):
-    return model
+@app.post("/translate", response_model=TaskResponse)
+def translate(request: TranslationRequest):
+    TaskResponse.task_id = 1
+    return TaskResponse
+
