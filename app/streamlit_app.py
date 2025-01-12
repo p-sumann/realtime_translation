1d964addb7ba5a96aabf2277daf2afb80e38a8da
Suman Paudel <dastonsuman1997@gmail.com>
2025-01-12 23:58:29 +0545

    ui for text and languages input

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -2,19 +2,33 @@
 import streamlit as st
 import requests
 
-# Define the FastAPI endpoint
-url_input = 'http://127.0.0.1:8000/inputs'
-model_input = 'http://127.0.0.1:8000/models'
+url = 'http://127.0.0.1:8000/translate'
 
-
-inputs = st.text_area("Enter Text")
-st.text_input(label= "Languages that wanna translate to",placeholder='e.g en, fr, de')
+text = st.text_area("Enter Text")
+lang = st.text_input(label= "Languages that wanna translate to",placeholder='e.g en, fr, de')
+lang = lang.split(',')
+data = {
+    'text' : text,
+    'languages' : lang
+}
 
 models_btn = st.button('Translate')
 if models_btn:
-    url = f'http://127.0.0.1:8000/models/{inputs}'
-    res = response = requests.get(url)
+    res = response = requests.post(url, json=data)
     heloo = str(res.json())
     st.write(heloo)
 
-st.text_input("Check Transition ID", placeholder='Enter translation id')
+# st.text_input("Check Transition ID", placeholder='Enter translation id')
+# col1, col2 = st.columns(2)
+
+# with col1:
+#     check_status_btn = col1.button('Check Status', key='status')
+
+# with col2:
+#     check_translation_btn = col2.button('Check Translation', key='translation')
+
+# if check_status_btn:
+#     st.write("Status button clicked")
+
+# if check_translation_btn:
+#     st.write("Translation button clicked")
